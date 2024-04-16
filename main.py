e088215b773d20265d3270e46bd80c2bd076840c
JS7077 <139284744+JS7077@users.noreply.github.com>
2024-04-16 02:35:15 +0000

    yay

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ import time
 
 
 #list of variables
-        
-a = ''#answer
-player_name = ''#name of player
-c = ',nbv'#constant unless player says so
-m = 100#money
-quest = ''#what quest the player is on
-php = 20#player current health
-pdmg = 1#player damage
-hp_pot = 0#healing potions owned
-str_pot = 0#strength potions owned
-b = ''#alternate answer
-n = 0#math variable
-score = 0#player's score
-quest1 = ''#other quest
-max_hp = 20#player max health
-real_score = score + max_hp + pdmg * 5 + hp_pot * 5 + str_pot * 5 + m / 2
-#global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+
+a = '' #answer
+b = '' #alternate answer
+c = ',nbv' #loop break
+m = 100 #money
+n = 0 #math variable
+player_name = '' #name of player
+quest = '' #what quest the player is on
+quest1 = '' #other quest
+php = 20 #player current health
+max_hp = 20 #player max health
+pdmg = 1 #player damage
+hp_pot = 0 #healing potions owned
+str_pot = 0 #strength potions owned
+score = 0 #player's score but not
+real_score = score + max_hp + pdmg * 5 + hp_pot * 5 + str_pot * 5 + m / 2 #player's displayed score
+
 
 
 #functions
 
 def query(question, answers):
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+    global a
     while True:
         print('', question, '')
         print(' Choose one of:', ', '.join(answers))
@@ -42,7 +42,7 @@ def query(question, answers):
             print('Invalid response')
 
 def heal(amount='0'):
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+    global php, max_hp
     amount = int(amount)
     php = php + amount
     if php > max_hp:
@@ -51,7 +51,7 @@ def heal(amount='0'):
     print('You gained', str(amount), 'health. You are now at', str(php), 'health.')
 
 def money(cost='0'):
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+    global m
     cost = int(cost)
     costt = 0
     if cost > m:
@@ -63,15 +63,14 @@ def money(cost='0'):
         m = costt
 
 def say(message='', talker=''):
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+  global player_name
+  if talker == '':
+    print(player_name + ':"' + message + '"')
+  else:
     print(talker + ':"' + message + '"')
 
-def psay(message=''):
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
-    say(message, player_name)
-
 def shop():
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+    global a, b, c, n, pdmg, max_hp, hp_pot, str_pot
     c = 'sg'
     while c != '':
         c = 'sf'
@@ -175,12 +174,12 @@ def shop():
             c = ''
 
 def town_hall():
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+    global a, quest1, score, player_name
     print('You enter the town hall.')
     if quest1 == 'Mayor':
         print('The mayor is inside.')
         if score > 45:
-            psay('I killed those monsters.')
+            say('I killed those monsters.')
             say('Thank you.', 'Mayor')
             money(-100)
             score = score + 50
@@ -197,101 +196,101 @@ def town_hall():
             print('Mayor:"You look like an adventurer."')
             say('What is your name?', 'Mayor')
             time.sleep(.5)
-            psay(player_name + '.')
+            say(player_name + '.')
             time.sleep(.5)
             print('Mayor:"Hello', player_name + '."')
             print('Mayor:"I have a quest for you."')
             a = query('Do you want to (1)Accept or (2)Reject?', ['1', '2'])
             if a == '1':
-                psay('I accept.')
+                say('I accept.')
                 time.sleep(.5)
                 quest1 = 'Mayor'
                 say('Monsters are terrorizing my town.', 'Mayor')
                 say('Kill them for me.', 'Mayor')
             else:
-                print(player_name + ':"Sorry."')
+                say('Sorry.')
     time.sleep(.5)
     print('You leave the town hall.')
 
 def tavern():
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+    global a, quest, pdmg
     print('You enter the tavern.')
     time.sleep(.5)
     print('Bartender:"Hail traveler."')
     money()
     a = query('Do you want to (1)Drink, (2)Eat, (3)Do both, or (4)Do neither?', ['1', '2', '3', '4'])
     if a == '1':
-        print(player_name + ':"Drink please."')
+        say("Drink please.")
         a = query('(1)Mead($5), (2)Cider($5), (3)Wine($10), or (4)Beer($10)?', ['1', '2', '3', '4'])
         if a == '1':
-            psay('Mead please.')
+            say('Mead please.')
             money(5)
             say('Here\'s your mead.', 'Bartender')
         elif a == '2':
-            psay('Cider please.')
+            say('Cider please.')
             money(5)
             say('Here\'s your cider.', 'Bartender')
         elif a == '3':
-            psay('Wine please.')
+            say('Wine please.')
             money(10)
             say('Here\'s your wine.', 'Bartender')
         elif a == '4':
-            psay('Beer please.')
+            say('Beer please.')
             money(10)
             say('Here\'s your beer.', 'Bartender')
         heal(5)
     elif a == '2':
-        print(player_name + ':"Food please."')
+        say("Food please.")
         a = query('(1)Stew($10), (2)Bread($5), or (3)Cheese($5)?', ['1', '2', '3'])
         if a == '1':
-            psay('Stew please.')
+            say('Stew please.')
             money(10)
             say('Here\'s your stew.', 'Bartender')
         elif a == '2':
-            psay('Bread please.')
+            say('Bread please.')
             money(5)
             say('Here\'s your bread.', 'Bartender')
         elif a == '3':
-            psay('Cheese please.')
+            say('Cheese please.')
             money(5)
             say('Here\'s your cheese.', 'Bartender')
         heal(10)
     elif a == '3':
-        print(player_name + ':"Both please."')
+        say("Both please.")
         a = query('(1)Stew($10), (2)Bread($5), or (3)Cheese($5)?',
                   ['1', '2', '3'])
         if a == '1':
-            psay('Stew please.')
+            say('Stew please.')
             money(10)
             say('Here\'s your stew.', 'Bartender')
         elif a == '2':
-            psay('Bread please.')
+            say('Bread please.')
             money(5)
             say('Here\'s your bread.', 'Bartender')
         elif a == '3':
-            psay('Cheese please.')
+            say('Cheese please.')
             money(5)
             say('Here\'s your cheese.', 'Bartender')
         a = query('(1)Mead($5), (2)Cider($5), (3)Wine($10), or (4)Beer($10)?', ['1', '2', '3', '4'])
         if a == '1':
-            psay('Mead please.')
+            say('Mead please.')
             money(5)
             say('Here\'s your mead.', 'Bartender')
         elif a == '2':
-            psay('Cider please.')
+            say('Cider please.')
             money(5)
             say('Here\'s your cider.', 'Bartender')
         elif a == '3':
-            psay('Wine please.')
+            say('Wine please.')
             money(10)
             say('Here\'s your wine.', 'Bartender')
         elif a == '4':
-            psay('Beer please.')
+            say('Beer please.')
             money(10)
             say('Here\'s your beer.', 'Bartender')
         heal(15)
     else:
-        print(player_name + ':"Nothing. Thank you."')
+        say("Nothing. Thank you.")
     if quest == 'Jerry':
         say('Wow, look at you.', 'Bartender')
         say('I\'ll go get that weapon.', 'Bartender')
@@ -299,7 +298,7 @@ def tavern():
         pdmg = pdmg + 1
         say('Here you go.', 'Bartender')
         time.sleep(.5)
-        psay('Thank you!')
+        say('Thank you!')
         quest = 'Complete'
     elif quest == 'Bartender':
         say('You haven\'t talked to my friend yet.', 'Bartender')
@@ -308,66 +307,66 @@ def tavern():
     else:
         a = query('Bartender:"Do you have some time?"', ['yes', 'no'])
         if a == 'yes':
-            psay('Yes.')
+            say('Yes.')
             time.sleep(.5)
             say('Great.', 'Bartender')
             say('I have a friend, Jerry, who lives down the road.', 'Bartender')
             say('Talk to him.', 'Bartender')
             quest = 'Bartender'
         else:
-            psay('No.')
+            say('No.')
             time.sleep(.5)
             say('Whatever works for you.', 'Bartender')
     time.sleep(.5)
     print('After a while you leave the tavern.')
 
 def inn():
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+    global a
     print('You enter the inn.')
     say('Hello.', 'Innkeeper')
     a = query('Innkeeper:"Is there anything I can help you with?"', ['yes', 'no'])
     if a == 'yes':
-        psay('Yes.')
+        say('Yes.')
         say('What, may I ask, is that.', 'Innkeeper')
         a = query('(1)I want a room, (2)Idk what to do, (3)You may not', ['1', '2', '3'])
         if a == '1':
-            say('I want a place to stay.', player_name)
+            say('I want a place to stay.')
             time.sleep(.5)
             say('You\'ve come to the right place.', 'Innkeeper')
             say('I can get you a Room and food($30) or just a Room($20).', 'Innkeeper')
             a = query('(1)Room and food or (2)Just a room?', ['1', '2'])
             if a == '1':
-                say('Room and food please.', player_name)
+                say('Room and food please.')
                 print('You eat and sleep in the inn and wake up feeling refreshed.')
                 money(30)
                 heal(999)
             else:
-                psay('Just a room.')
+                say('Just a room.')
                 print('You sleep in the inn and wake up feeling refreshed but hungry.')
                 money(20)
                 heal(99)
             say('Come again if you need a place to sleep again.', 'Innkeeper')
         elif a == '2':
-            say('Idk what to do.', player_name)
+            say('Idk what to do.')
             say('Go to the town hall and ask the mayor.', 'Innkeeper')
         else:
-            psay('You may not.')
+            say('You may not.')
             say('So rude.', 'Innkeeper')
     else:
-        psay('No.')
+        say('No.')
         say('Ok then.', 'Innkeeper')
     time.sleep(.5)
     print('You leave the inn.')
 
 def jerry():
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+    global a, c, player_name, quest, max_hp
     if a == 'yes':
         print('You go down the road and find him.')
         print('Stranger:"Hello there,', player_name + '."')
         print('Stranger:"You must be the one the bartender was talking about."')
         a = query('(1)Are you Jerry, (2)How do you know my name?', ['1', '2'])
         if a == '1':
-            psay('Are you Jerry?')
+            say('Are you Jerry?')
             say('Yes I am.', 'Jerry')
             say('The bartender told me to give you some armor.', 'Jerry')
             say('Here you go.', 'Jerry')
@@ -377,16 +376,16 @@ def jerry():
             quest = 'Jerry'
             c = ''
         else:
-            psay('How do you know my name?')
+            say('How do you know my name?')
             say('I have my ways.', 'Stranger')
             print('You walk away from the creep.')
-            
+
 def combat(enemy='', hp='5', dmg='1', bvus='-1'):
     #variables
     enemy_list = ['goblin', 'bandit', 'slime', 'orc', 'slime', 'slime', 'goblin', 'bandit', ]
     enemy_hp = int(hp)
     enemy_dmg = int(dmg)
-    global a, b, c, m, n, player_name, quest, quest1, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
+    global a, b, c, m, php, pdmg, max_hp, hp_pot, str_pot, score, real_score
     #start
     if enemy == '':
         enemy = random.choice(enemy_list)
@@ -501,24 +500,26 @@ def combat(enemy='', hp='5', dmg='1', bvus='-1'):
             c = ''
             sys.exit()
     pdmg = b
-    
+
 
 #start of program
 print('Ready player n.')
 print()
 time.sleep(1)
 #start of game
+#intro
 player_name = input(' What is your name?:')
 say('Welcome traveler.', 'Dangalf')
 a = query('Dangalf:"Do you want to go on an adventure?"', ['yes', 'no'])
 if a == 'yes':
-    psay('Yes.')
+    say('Yes.')
     say('Good.', 'Gandalf')
 else:
-    psay('No.')
+    say('No.')
     say('Well too bad.', 'Gandalf')
 print()
 time.sleep(1)
+#town
 print('You come to a town.')
 a = query('Do you want to go in?', ['yes', 'no'])
 if a == 'no':
@@ -541,14 +542,14 @@ while True:
         time.sleep(.5)
         a = query('Do you want to leave the town?', ['yes', 'no'])
         c = a
-        
+
     if a != 'sgsbr':
         print('You leave the town.')
-        
+
     if quest == 'Bartender':
         a = query('Do you want to go to the Bartender\'s friend?', ['yes', 'no'])
         jerry()
-        
+
     while c != '':
         combat()
         a = query('Do you want to return to the town?', ['yes', 'no'])
